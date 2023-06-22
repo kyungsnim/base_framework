9dffa107efe828b1ed6fcb154ca8b0e7502d5d0f
kyungsnim <skyboom86@daum.net>
2023-06-22 09:34:21 +0900

    update utils, widgets

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primaryColor: kIconColor,
+        indicatorColor: kIndicatorColor,
+        bottomSheetTheme: BottomSheetThemeData(
+          backgroundColor: kIconColor,
+        ),
         useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black87)
+            .copyWith(background: kBackgroundColor),
       ),
       home: HomePresenter(),
     );
